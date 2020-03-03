9db7959a6a4bf6c6d85b3de3d900524d7198a401
morfeu9 <gabriel_nogueira21@hotmail.com>
2020-03-03 02:38:58 -0300

    adicionando dependencias do spring-boot-admin, adicinando travis.ci no projeto

--- src/main/java/com/apirestlibrary/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/apirestlibrary/libraryapi/LibraryApiApplication.java
@@ -1,5 +1,6 @@
 package com.apirestlibrary.libraryapi;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAdminServer
 public class LibraryApiApplication extends SpringBootServletInitializer {
 /*
 	@Autowired
